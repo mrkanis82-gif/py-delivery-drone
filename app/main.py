4981612d54b15b8252d46827e3eda869b256dc42
Tymur <mr.kanis82@gmail.com>
2026-03-16 18:46:51 -0400

    finished

--- app/main.py
+++ app/main.py
@@ -53,8 +53,8 @@ class DeliveryDrone(FlyingRobot):
                  name: str,
                  weight: int,
                  coords: list | None = None,
-                 max_load_weight: int = None,
-                 current_load: Cargo = None
+                 max_load_weight: int | None = None,
+                 current_load: Cargo | None = None
                  ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
